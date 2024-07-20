ec04a1cb8722dc166a683a5f45f58b6e3392d973
Влад Фомин <58701528+vladhog@users.noreply.github.com>
2024-07-20 23:51:07 +0300

    changed addons paths

--- sieloader.py
+++ sieloader.py
@@ -3,10 +3,10 @@ import subprocess
 
 # first, searching for sierra
 sierra_path = sierra_search()
-addons_dir = sierra_path + "/addons/"
+addons_dir = "./addons/"
 
 # second, check if addons folder exist, and if is, load addons from there
-if os.path.isdir(sierra_path + "/addons/"):
+if os.path.isdir("./addons/"):
     addons = get_addons(addons_dir)
     # Now, lets check for install scripts
     for addon in addons:
@@ -15,7 +15,7 @@ if os.path.isdir(sierra_path + "/addons/"):
     # after we're done running installation scripts, we make universal addon file which user will load
     # and to make it easier, we will also open where file stored
     universal_addon_file = generate_addon_file(sierra_path, addons_dir, addons)
-    subprocess.run([os.path.join(os.getenv('WINDIR'), 'explorer.exe'), '/select,', os.path.abspath(universal_addon_file)])
-    subprocess.run([os.path.join(sierra_path, 'SIERRA.exe')])
+    subprocess.Popen([os.path.join(os.getenv('WINDIR'), 'explorer.exe'), '/select,', os.path.abspath(universal_addon_file)], start_new_session=True)
+    subprocess.Popen([os.path.join(sierra_path, 'SIERRA.exe')], start_new_session=True)
 else:
-    os.mkdir(sierra_path + "/addons/")
+    os.mkdir("./addons/")
